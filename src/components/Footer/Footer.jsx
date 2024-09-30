d6f66e772a735395f431ce4ac107afa032ff9b64
Amala TV <121046726+Amalatv@users.noreply.github.com>
2024-09-30 17:22:49 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
-
-import fbIcon from '../../assets/facebook.png';
-import twitterIcon from '../../assets/twitter.png';
-import instagramIcon from '../../assets/instagram.png'
 import { Box, Container, styled, Typography } from '@mui/material';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
@@ -10,72 +6,53 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Footer = () => {
 
-  const  CustomContainer = styled(Container)(({theme}) => ({
-   display: "flex",
-   justifyContent: "space-around",
-   gap: theme.spacing(5),
-   
-   [theme.breakpoints.down("md")]: {
-    flexDirection:'column',
-    alignItems:'center',
-    justifyContent:'center',
+  const CustomContainer = styled(Container)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "space-around",
+    gap: theme.spacing(5),
+    [theme.breakpoints.down("md")]: {
+      flexDirection: 'column',
+      alignItems: 'flex-start', 
+      justifyContent: 'center',
+    }
+  }));
 
-   }
-  }))
+  const IconBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    gap: ".5rem",
+    [theme.breakpoints.down("sm")]: {
+      justifyContent: "flex-start", 
+    },
+  }));
 
-  const IconBox = styled(Box)(({theme}) => ({
-     display:"flex",
-     gap: ".5rem",
-     [theme.breakpoints.down("sm")]: {
-     justifyContent:"center",
-     },
+  const FooterLink = styled("span")(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    gap: ".5rem",
+    [theme.breakpoints.down("md")]: {
+      alignItems: "flex-start", 
+      justifyContent: 'flex-start',
+    }
+  }));
 
-  }))
-
-  const FooterLink = styled("span")(({theme}) => ({
-   display: "flex",
-   flexDirection: "column",
-   gap: ".5rem",
-   [theme.breakpoints.down("md")]: {
-    alignItems:"center",
-    justifyContent:'center',
-
-   }
-  //  fontSize: "14px",
-  //  color: "black",
-  //  fontWeight: "bold",
-  //  cursor: "pointer",
-  //  "&:hover": {
-  //    color: "#fff",
-  //  },
-  }))
-
-
+  const CenterFooterLink = styled(FooterLink)(({ theme }) => ({
+    [theme.breakpoints.down("md")]: {
+      alignItems: "flex-start", 
+      justifyContent: 'flex-start',
+    }
+  }));
 
   const featured_list = [
-    {path: "/" , 
-      display: "Guides"
-    },
-    {path: "/" , 
-      display: "Services"
-    },
-    {path: "/" , 
-      display: "Contact us"
-    },
+    { path: "/", display: "Guides" },
+    { path: "/", display: "Services" },
+    { path: "/", display: "Contact us" },
   ];
 
   const overview_list = [
-    {path: "/" , 
-      display: "Location"
-    },
-    {path: "/" , 
-      display: "Partnerships"
-    },
-    {path: "/" , 
-      display: "Terms of use & Privacy Policies"
-    },
+    { path: "/", display: "Location" },
+    { path: "/", display: "Partnerships" },
+    { path: "/", display: "Terms of use & Privacy Policies" },
   ];
-
 
   const Link = styled(Typography)(() => ({
     fontSize: "16px",
@@ -85,87 +62,78 @@ const Footer = () => {
     "&:hover": {
       color: "#66B2FF",
     },
-  }))
+  }));
 
   return (
-    <Box sx={{py: 10, backgroundColor: "#FFF6B2"}}>
-
+    <Box sx={{ py: 10, backgroundColor: "#FFF6B2" }}>
       <CustomContainer>
         <Box>
-             <Typography
-             sx={{
+          <Typography
+            sx={{
               fontSize: "20px",
               color: "1C1C1D",
               fontWeight: "700",
               mb: 2,
-             }}
-             >
-              Featured
-             </Typography>
-             <FooterLink>
-               {featured_list.map((item, index) => (
-                <Link>{item.display}</Link>
-               ))}
-             </FooterLink>
-        </Box>
-      
-        <Box>
-        <Typography
-             sx={{
-              fontSize: "20px",
-              color: "1C1C1D",
-              fontWeight: "700",
-              mb: 2,
-             }}
-             >
-              Overview
-             </Typography>
-             <FooterLink>
-               {overview_list.map((item, index) => (
-                <Link>{item.display}</Link>
-               ))}
-             </FooterLink>
+            }}
+          >
+            Featured
+          </Typography>
+          <CenterFooterLink>
+            {featured_list.map((item, index) => (
+              <Link key={index}>{item.display}</Link>
+            ))}
+          </CenterFooterLink>
         </Box>
 
         <Box>
           <Typography
-             sx={{
+            sx={{
               fontSize: "20px",
               color: "1C1C1D",
               fontWeight: "700",
               mb: 2,
-             }}
-             >
-              Get in touch
-             </Typography>
-             <Typography
-                        sx={{
-                          fontSize: "16px",
-                          color: "#000066",
-                          fontWeight: "500",
-                          mb: 2,
-                         }}
-             >
-               Keep in touch with our social media pages.
-             </Typography>
-            
-             <IconBox >
-               {/* {overview_list.map((item, index) => (
-                <Link>{item.display}</Link>
-               ))} */}
-                <FacebookIcon/>
-                <InstagramIcon/>
-                <TwitterIcon/>
-             </IconBox>
-             
-             <IconBox>
+            }}
+          >
+            Overview
+          </Typography>
+          <FooterLink>
+            {overview_list.map((item, index) => (
+              <Link key={index}>{item.display}</Link>
+            ))}
+          </FooterLink>
+        </Box>
 
-             </IconBox>
+        <Box>
+          <Typography
+            sx={{
+              fontSize: "20px",
+              color: "1C1C1D",
+              fontWeight: "700",
+              mb: 2,
+            }}
+          >
+            Get in touch
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: "16px",
+              color: "#000066",
+              fontWeight: "500",
+              mb: 2,
+            }}
+          >
+            Keep in touch with our social media pages.
+          </Typography>
+
+          <IconBox>
+            <FacebookIcon />
+            <InstagramIcon />
+            <TwitterIcon />
+          </IconBox>
         </Box>
       </CustomContainer>
-     
     </Box>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
